251ef47cee7c0211f7c154dd44b83f3a7b4cd8d1
giswarya <giswarya@progress.com>
2021-01-04 19:34:08 +0530

    jj

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -44,7 +44,7 @@ object Build : BuildType({
 
     steps {
         script {
-                scriptContent = """echo "helloworld" >sampletext.txt"""
+                scriptContent = """echo "hello" >sampletext.txt"""
         }
 
     }
